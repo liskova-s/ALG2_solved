e2ffdc5b151d88d66e240aed99dceee9e12ee63e
liskova-s <sarkaliskov@gmail.com>
2021-03-05 10:53:35 +0100

    String recognition implementation, smooth for common fractions

--- 15_Liskova_Fractions/Fractions.java
+++ 15_Liskova_Fractions/Fractions.java
@@ -5,77 +5,93 @@ package Fraction;
  *
  * @author sarka
  */
-public class Fraction {
+ public class Fraction {
 
-    //data
-    private int num;
-    private int denom;
+     //data
+     private int num;
+     private int denom;
 
-    public Fraction(double num, double denom) {         //converting numerator and denominator into integer form
-        boolean end = false;
-        if(denom==0){
-            throw new IllegalArgumentException("Denominator equals 0");
-        }
-        while (!end) {
-            if ((num * 10) % 10 == 0 && (denom * 10) % 10 == 0) {
-                this.num = (int) num;
-                this.denom = (int) denom;
-                end = true;
-            } else {
-                num = num * 10;
-                denom = denom * 10;
-            }
-        }
-        //System.out.println(this.num);
-        //System.out.println(this.denom);
+     public Fraction(double num, double denom) {         //converting numerator and denominator into integer form
+         this.num = integerSwitch(num, denom)[0];
+         this.denom = integerSwitch(num, denom)[1];
 
-    }
+     }
 
+     private static int[] integerSwitch(double num, double denom) {
+         int[] integerSwitch = new int[2];
+         boolean end = false;
 
-    //methods
-    public int getNumerator() {
-        return num;
-    }
+         while (!end) {
+             if ((num * 10) % 10 == 0 && (denom * 10) % 10 == 0) {
+                 integerSwitch[0] = (int) num;
+                 integerSwitch[1] = (int) denom;
+                 end = true;
+             } else {
+                 num = num * 10;
+                 denom = denom * 10;
+             }
+         }
+         if (denom == 0) {
+             throw new IllegalArgumentException("Denominator equals 0");
+         }
+         return integerSwitch;
+     }
 
-    public int getDenominator() {
-        return denom;
-    }
+     public Fraction(String fract) {
+         String[] f = fract.split("/");
+         this.num = integerSwitch(Double.parseDouble(f[0]), Double.parseDouble(f[1]))[0];
+         this.denom = integerSwitch(Double.parseDouble(f[0]), Double.parseDouble(f[1]))[1];
+     }
 
-    public String fractionToString() {
-        return num + "/" + denom;
-    }
+     //methods
+     public int getNumerator() {
+         return num;
+     }
 
-    public int[] getSimplestFraction() {
-        int [] simplestFraction = new int[2];
-        int gcd = getGCD(num, denom);
-        if (gcd > 1) {
-            simplestFraction[0]= num / gcd;
-            simplestFraction[1] = denom / gcd;
-        } else{
-         simplestFraction[0]= num;
-         simplestFraction[1] = denom;
-        }
-        return simplestFraction;
-    }
+     public int getDenominator() {
+         return denom;
+     }
 
-    public int getNumOfSF(){
-        int [] simplestFraction = getSimplestFraction();
-        return simplestFraction[0];
-    }
+     public String fractionToString() {
+         return num + "/" + denom;
+     }
 
-    public int getDenomOfSF(){
-        int [] simplestFraction = getSimplestFraction();
-        return simplestFraction[1];
-    }
+     public int[] getSimplestFraction() {
+         int[] simplestFraction = new int[2];
+         int gcd = getGCD(num, denom);
+         if (gcd > 1) {
+             simplestFraction[0] = num / gcd;
+             simplestFraction[1] = denom / gcd;
+         } else {
+             simplestFraction[0] = num;
+             simplestFraction[1] = denom;
+         }
+         return simplestFraction;
+     }
 
-    private int getGCD(int x, int y) {
-        int r;
-        while(y!=0){
-            r=x%y;
-            x=y;
-            y=r;
-        }
-        return x;
-    }
+     public int getNumOfSF() {
+         int[] simplestFraction = getSimplestFraction();
+         return simplestFraction[0];
+     }
 
-}
+     public int getDenomOfSF() {
+         int[] simplestFraction = getSimplestFraction();
+         return simplestFraction[1];
+     }
+
+     private int getGCD(int x, int y) {
+         int r;
+         while (y != 0) {
+             r = x % y;
+             x = y;
+             y = r;
+         }
+         return x;
+     }
+
+     public void simplify() { //not static, depends on object(instance) Fraction
+         num = getNumOfSF();
+         denom = getDenomOfSF();
+     }
+
+ }
--- 15_Liskova_Fractions/FractionsApp.java
+++ 15_Liskova_Fractions/FractionsApp.java
@@ -14,21 +14,26 @@ public class FractionApp {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        Fraction f = new Fraction(12.568, 2);
+     public static void main(String[] args) {
+         //Fraction f = new Fraction("2/4");
+         Fraction f = new Fraction(16.2, 45);
 
-        //return numerator:
-        System.out.println(f.getNumerator());
+         //return numerator:
+         System.out.println("Numerator: " + f.getNumerator());
 
-        //return denominator:
-        System.out.println(f.getDenominator());
+         //return denominator:
+         System.out.println("Denominator: " + f.getDenominator());
 
-        //String interpretation:
-        System.out.println(f.fractionToString());
+         //String interpretation:
+         System.out.println("String interpretation: " + f.fractionToString());
 
-        //Simplify fraction:
-        Fraction s = new Fraction(f.getNumOfSF(), f.getDenomOfSF());
-        System.out.println(s.fractionToString());
-    }
+         //Simplify fraction:
+         f.simplify();
+         System.out.println("Simplest form: " + f.fractionToString());
 
+         //Create new simplified fraction 
+         /*
+         Fraction s = new Fraction(f.getNumOfSF(), f.getDenomOfSF());
+         System.out.println("Simplest form: "+s.fractionToString());*/
+     }
 }
